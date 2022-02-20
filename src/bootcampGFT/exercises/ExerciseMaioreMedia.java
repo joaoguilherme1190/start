37e17838c9c8733c87434b0df6b7425d75459975
guilherme <jgt111190@gmail.com>
2022-02-20 20:45:21 -0300

    test

--- src/bootcampGFT/exercises/ExerciseMaioreMedia.java
+++ src/bootcampGFT/exercises/ExerciseMaioreMedia.java
@@ -7,6 +7,7 @@ public class ExerciseMaioreMedia {
     public static void main(String[] args) {
 
         System.out.println("Hello World !");
+        System.out.println("Teste commit");
 
     }
 }
